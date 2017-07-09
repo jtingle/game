1375e355f501c71d1a47139d2530569def46b651
Joe <jetingle@outlook.com>
2017-07-09 13:24:31 -0400

    section 2 lecture 6

--- game.js
+++ game.js
@@ -1,19 +1,36 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 10;
 
 window.onload = function() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
-  setInterval(drawEverything, 50);
+  var framesPerSecond = 30;
+  setInterval(function() {
+    moveEverything();
+    drawEverything();
 
+  }, 1000/framesPerSecond);
+}
+
+
+function moveEverything () {
+  ballX = ballX + ballSpeedX;
+  if(ballX < 0){
+        ballSpeedX = -ballSpeedX;
+  }
+  if(ballX > canvas.width){
+        ballSpeedX = -ballSpeedX;
+  }
 }
 
 function drawEverything() {
-  ballX = ballX + 20;
-  console.log("ballX"); //this allows us to see function has run
+
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(0,210,10,100);
   canvasContext.fillStyle = 'red';
   canvasContext.fillRect(ballX,100,10,10);
 
