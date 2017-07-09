69ddee54433dadefa655380962d778a741f7bd73
Joe <jetingle@outlook.com>
2017-07-09 14:01:45 -0400

    left padlles moves

--- game.js
+++ game.js
@@ -1,7 +1,26 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY =50;
 var ballSpeedX = 10;
+var ballSpeedY = 4;
+
+var paddle1Y = 250;
+const PADDLE_HEIGHT = 100;
+
+
+function calculateMousePos(evt) {
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top - root.scrollTop;
+  return {
+    x:mouseX,
+    y:mouseY
+
+  };
+
+}
 
 window.onload = function() {
   canvas = document.getElementById('gameCanvas');
@@ -12,26 +31,51 @@ window.onload = function() {
     drawEverything();
 
   }, 1000/framesPerSecond);
+
+  canvas.addEventListener('mousemove',
+    function(evt) {
+        var mousePos = calculateMousePos(evt);
+        paddle1Y = mousePos.y-(PADDLE_HEIGHT/2);
+    });
 }
 
 
 function moveEverything () {
   ballX = ballX + ballSpeedX;
+  ballY = ballY + ballSpeedY;
   if(ballX < 0){
         ballSpeedX = -ballSpeedX;
   }
   if(ballX > canvas.width){
         ballSpeedX = -ballSpeedX;
   }
+
+  if(ballY < 0){
+        ballSpeedY = -ballSpeedY;
+  }
+  if(ballY > canvas.height){
+        ballSpeedY = -ballSpeedY;
+  }
 }
 
 function drawEverything() {
+  //next line blanks out the screen with blank
+  colorRect(0,0,canvas.width,canvas.height,'black');
+  //this is left player paddle
+  colorRect(0,paddle1Y,10,100,'white');
+    //next line draws the ball
+    colorCircle(ballX, ballY, 10, 'white');
+}
 
-  canvasContext.fillStyle = 'black';
-  canvasContext.fillRect(0,0,canvas.width,canvas.height);
-  canvasContext.fillStyle = 'white';
-  canvasContext.fillRect(0,210,10,100);
-  canvasContext.fillStyle = 'red';
-  canvasContext.fillRect(ballX,100,10,10);
+function colorCircle(centerX, centerY, radius, drawColor) {
+  canvasContext.fillStyle = 'drawColor';
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2, true);
+  canvasContext.fill();
+}
+
+function colorRect(leftX,topY, width,height, drawColor ) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX,topY, width,height);
 
 }
